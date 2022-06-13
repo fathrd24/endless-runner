aebd2206da5a08b104ffe0964f19456dab569a15
fathrd24 <fathrd24@gmail.com>
2022-06-13 12:54:34 +0700

    camera Follow
    add camera move controller

---
+++ Assets/scripts/cameraMoveController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMoveController : MonoBehaviour
+{
+    [Header("position")]
+    public Transform player;
+    public float horizontaloffset;
+
+    private void Update()
+    {
+        Vector3 newPosition = transform.position;
+        newPosition.x = player.position.x + horizontaloffset;
+        transform.position = newPosition;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+}
+    // Update is called once per frame
+    
